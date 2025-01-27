4082da8db7de5c11dc95c4b276a18708b57b5991
pyro-42 <dev.h8671@slmails.com>
2025-01-26 22:02:26 -0800

    New starting setup done

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import { defineConfig } from 'vite';
-import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
 
 export default defineConfig({
-	plugins: [
-		nodePolyfills({
-			exclude: ['fs'],
-			globals: {
-				Buffer: true,
-				global: true,
-				process: true
-			},
-			protocolImports: true
-		}),
-		sveltekit()
-	],
-	build: {
-		commonjsOptions: {
-			transformMixedEsModules: true
-		}
-	},
-	optimizeDeps: {
-		esbuildOptions: {
-			plugins: [esbuildCommonjs(['@wagmi/core'])]
-		}
-	}
+	plugins: [sveltekit()]
 });
